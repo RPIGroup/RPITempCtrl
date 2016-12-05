3fae457a70a74064797c5aaf38f9cb3b2abc95a0
maobaolong <maobaolong@jd.com>
2016-12-05 11:44:27 +0800

    加入gpio的输出

--- RPITempCtrl-core/src/main/java/net/rpi/App.java
+++ RPITempCtrl-core/src/main/java/net/rpi/App.java
@@ -1,13 +1,26 @@
 package net.rpi;
 
+import com.pi4j.io.gpio.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final GpioController gpio = GpioFactory.getInstance();
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "" , PinState.LOW);
+        try {
+            myLed.high();
+            Thread.sleep(500);
+            myLed.low();
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
