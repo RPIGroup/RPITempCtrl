3edcbc05fd9d4e66fc64a1ff8691b357ec7196a0
mbl <maobaolong@139.com>
2016-12-08 23:54:20 +0800

    增加进程id

--- RPITempCtrl-core/src/main/java/net/rpi/common/SystemInfoUtils.java
+++ RPITempCtrl-core/src/main/java/net/rpi/common/SystemInfoUtils.java
@@ -2,6 +2,8 @@ package net.rpi.common;
 
 import net.rpi.entity.SystemInfoEntity;
 
+import java.lang.management.ManagementFactory;
+
 /**
  * Created by maobaolong on 2016/12/7.
  * 获取系统信息
@@ -17,10 +19,16 @@ public class SystemInfoUtils {
         return Integer.parseInt(content);
     }
 
+    public static int getPid(){
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        String pid = name.split("@")[0];
+        return Integer.valueOf(pid);
+    }
     public static SystemInfoEntity getDefault(){
         return DEFAULT;
     }
     public static void refresh(){
         DEFAULT.setTemperature(getTemperature());
+        DEFAULT.setPid(getPid());
     }
 }
